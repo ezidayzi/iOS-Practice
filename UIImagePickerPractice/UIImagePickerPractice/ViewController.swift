0ca6cd1c36af940b315858010868b275a967a665
김윤서 <yo26on@soongsil.ac.kr>
2021-05-22 13:58:36 +0900

    [ADD] UIImagePickerPractice Project

---
+++ UIImagePickerPractice/UIImagePickerPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIImagePickerPractice
+//
+//  Created by 김윤서 on 2021/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
