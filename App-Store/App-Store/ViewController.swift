1eaf833c5e2c739162928b94a41728ee0460bed0
Yoonseo Kim <yo26on@soongsil.ac.kr>
2022-01-23 14:32:07 +0900

    App-Store 프로젝트 생성

---
+++ App-Store/App-Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App-Store
+//
+//  Created by 김윤서 on 2022/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
